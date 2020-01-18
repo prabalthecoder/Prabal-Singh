b36f8a839aa0c266d2799084ce1aebf0563b41ea
prabal <prabalthecoder@gmail.com>
2020-01-18 14:54:55 +0530

    continue and break example

--- venv/break and continue.py
+++ venv/break and continue.py
@@ -1,10 +1,11 @@
-i = 0
-a = "helloasdfghjklw"
-while(True):
-    if (a[i] =="w" ):
-        break
+i = -1
+a = "hello world"
+while(i < len(a) - 1 ):
+    i = i + 1
+    if (a[i] =="o"):
+        continue
     print(a[i])
-    i = i +1
+
 print("done")
 
 
