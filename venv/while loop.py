0cd0a5578600295e443e535fdb17839dc2df507b
prabal <prabalthecoder@gmail.com>
2020-01-18 14:29:10 +0530

    example while loop

---
+++ venv/while loop.py
@@ -0,0 +1,26 @@
+a = [1, 2, 3, 4, 5, 6, 7]
+i = 0
+
+while(a[i] <= 5):
+    print(a[i])
+    i = i + 1
+print("hello")
+
+a = 5
+if(a % 2 == 0):
+    print("even no")
+else:
+    print("odd no")
+
+i = 4
+if (i == 1):
+    print("sparsh")
+
+elif (i == 2):
+    print("prabal")
+
+elif(i == 3):
+    print("dhruv")
+
+else:
+    print("sorry")
