63eef720d0a218c164dbb1f846efa9118653d466
prabal <prabalthecoder@gmail.com>
2020-01-19 14:17:19 +0530

    map functions example

---
+++ venv/map function example.py
@@ -0,0 +1,14 @@
+
+a = [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+def sqr(num):
+    return  num ** 2
+b = list(map(sqr, a))
+print(b)
+
+c = [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+d = list(map(lambda x : x ** 2, c))
+print(d)
+
+c = [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+d = list(map(lambda x : x % 2, c))
+print(d)
