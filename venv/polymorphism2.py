06dd9e9546ade17b7773c121393ab150708eaaef
prabal <prabalthecoder@gmail.com>
2020-03-05 22:10:14 +0530

    states and currency example

---
+++ venv/polymorphism2.py
@@ -0,0 +1,26 @@
+class India:
+    def states(self):
+        print(29)
+
+    def currency(self):
+        print("Rupee")
+
+
+class Usa:
+    def states(self):
+        print(15)
+
+    def currency(self):
+        print("Dollar")
+
+
+def main():
+    ind_obj = India()
+    usa_obj = Usa()
+
+    for obj in (ind_obj, usa_obj):
+        obj.states()
+        obj.currency()
+
+if __name__ == "__main__":
+    main()
