68dc9827ce8e6b675236cb6e4a436deb6a3ca770
prabal <prabalthecoder@gmail.com>
2020-03-18 22:03:04 +0530

    generators 2nd example

---
+++ venv/generetors_2.py
@@ -0,0 +1,15 @@
+def rev_str(my_str):
+    length = len(my_str)
+    for i in range(length - 1, -1, -1):
+        yield my_str[i]
+
+def main():
+    my_str = "Hello"
+
+    for char in rev_str(my_str):
+        print(char, end=", ")
+
+if __name__ == '__main__':
+    main()
+
+
